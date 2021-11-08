f134e6252b73db498f86bfaf208d3a9324bc542a
aliboy124 <aligreat124@gmail.com>
2021-11-09 00:28:06 +0500

    Created Main Page *added text, switch images and button *added constraints *added onClick methods

--- app/src/main/java/com/example/secondassignment/MainActivity.java
+++ app/src/main/java/com/example/secondassignment/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.secondassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView github = (ImageView) findViewById(R.id.github);
+        github.setVisibility(View.INVISIBLE);
+    }
+    public void goToApp(View view){
+        Intent intent = new Intent(MainActivity.this,AppHome.class);
+        startActivity(intent);
+    }
+    public void goToGithub(View view){
+        Intent intent = new Intent( Intent.ACTION_VIEW , Uri.parse("https://github.com/aliboy124/MC_Assignment_2") );
+        startActivity(intent);
+    }
+    public void switchImage(View view){
+        Switch sw = (Switch) findViewById(R.id.switch1);
+        ImageView github = (ImageView) findViewById(R.id.github);
+        if(sw.isChecked()){
+            github.setVisibility(View.VISIBLE);
+        }
+        else{
+            github.setVisibility(View.INVISIBLE);
+        }
+
     }
 }
